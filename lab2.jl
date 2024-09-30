a438ebeffbc28bbcbc7d422210c1270abeb2fed9
Hazel Brown <frostjotun@gmail.com>
2024-09-30 10:49:30 -0400

    finish lab 2 (hazel)

--- lab2.jl
+++ lab2.jl
@@ -1,4 +1,5 @@
-using Unitful, Plots, CurveFit
+import Pkg; Pkg.activate(".")
+using Unitful, Plots, CurveFit, LaTeXStrings
 
 # f1 = 383.5u"Hz"
 
@@ -15,13 +16,13 @@ zags1 = [18.5, 34, 67]
 
 # peaks2 = [19, 59]
 
-f2 = 440.8
+f2 = 440.8u"Hz"
 
 peaks2 = [4.5, 41.5, 67]
 
 zags2 = [15.75, ]
 
-f3 = 480.4
+f3 = 480.4u"Hz"
 
 peaks3 = [5.5, 40, 76.5]
 
@@ -32,5 +33,22 @@ peaks4 = [7.5, 41, 77.5]
 
 
 
-# fs = 
+fs = [f1,f2,f3]
+lams = [92,75,69] .* u"cm"
+lams_inv = uconvert.(u"m^-1", 1 ./ lams)
 
+reg = linear_fit(fs, lams_inv)
+
+scatter(fs, lams, show=true, label=false, xlabel="frequency", ylabel="wavelength", title="Resonant wavelengths for given frequencies")
+
+savefig("lab2-graph1.svg")
+
+readline()
+
+scatter(fs, lams_inv, show=true, label=false, xlabel="frequency", ylabel="inverse wavelength")
+
+plot!([f1,f3], [f1*reg[2]+reg[1], f3*reg[2]+reg[1]], label=L"1/\lambda = \frac{1.27}{v_{mean}}f - \frac{0.260}{\lambda_{mean}}", title="Linearization of wavelengths and frequencies")
+
+savefig("lab2-graph2.svg")
+
+readline()
